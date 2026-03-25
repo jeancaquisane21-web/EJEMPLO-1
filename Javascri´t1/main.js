9fd64ff01d57d9bd98a935e64481bced1d7871ad
jean <redsupp@hotmail.com>
2026-03-25 19:33:09 -0300

    first commit

---
+++ Javascri´t1/main.js
@@ -0,0 +1,15 @@
+console.log("javascript conectado correctamente");
+;
+const sum1 = 5
+const sum2 = 6
+
+
+console.log(sum1 * sum2)
+
+console.log("javascrpit es lool" .toUpperCase())
+
+
+
+
+
+console.log("hola=3".length)
